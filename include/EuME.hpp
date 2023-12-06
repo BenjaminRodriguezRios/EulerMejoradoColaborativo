e7b3809b71b493acdb2a2c4547fc25aad89345cc
BenjaminRodriguezRios <a22310233@live.ceti.mx>
2023-12-05 18:12:25 -0600

    arantza<3

--- include/EuME.hpp
+++ include/EuME.hpp
@@ -7,16 +7,4 @@ public:
     EuME(/* args */) {}
     ~EuME() {}
 };
-class DatosUsuario {
-public:
-    static void ingresarDatos(double& x0, double& y0, double& h, int& num_pasos) {
-        std::cout << "Ingrese x0: ";
-        std::cin >> x0;
-        std::cout << "Ingrese y0: ";
-        std::cin >> y0;
-        std::cout << "Ingrese el tamaño del paso (h): ";
-        std::cin >> h;
-        std::cout << "Ingrese el número de pasos: ";
-        std::cin >> num_pasos;
-    }
-};
+
--- src/EulerMejorado.cpp
+++ src/EulerMejorado.cpp
@@ -1,7 +1,19 @@
 //arantza y benjamin se aman 
 #include <iostream>
 #include <cmath>
-
+class DatosUsuario {
+public:
+    static void ingresarDatos(double& x0, double& y0, double& h, int& num_pasos) {
+        std::cout << "Ingrese x0: ";
+        std::cin >> x0;
+        std::cout << "Ingrese y0: ";
+        std::cin >> y0;
+        std::cout << "Ingrese el tamaño del paso (h): ";
+        std::cin >> h;
+        std::cout << "Ingrese el número de pasos: ";
+        std::cin >> num_pasos;
+    }
+};
 class Validacion {
 public:
     static void aplicarFix(double& h, int& num_pasos) {
